2f880860db5e7f56a67b30da36050e3b9bf7cc6e
marcosviudes <55087357+marcosviudes@users.noreply.github.com>
2020-11-15 08:59:16 +0100

    Server finished :)

--- srcs/init.sh
+++ srcs/init.sh
@@ -3,17 +3,16 @@ wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-language
 mv /phpMyAdmin-5.0.2-all-languages.zip /temp
 unzip temp/phpMyAdmin-5.0.2-all-languages.zip
 mv phpMyAdmin-5.0.2-all-languages /var/www/html/phpmyadmin
-#mv /temp/config.inc.php /var/www/html/phpmyadmin
 rm /var/www/html/phpmyadmin/config.sample.inc.php && mv /temp/config.inc.php /var/www/html/phpmyadmin/
-#rm phpMyAdmin-5.0.2-all-languages.zip
-#chmod -R 777 /var/www/html/phpmyadmin
 chmod -R 775 /var/www/html/phpmyadmin
 chown -R www-data:www-data /var/www/html/phpmyadmin
 echo "\033[1;32mphpmyadmin [OK]\033[0m"
 
+
 echo "Configure SSL"
 openssl genrsa -out /etc/ssl/private/nginx.key 4096
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=SP/ST=Spain/L=Madrid/O=42Madrid/CN=127.0.0.1" -keyout /etc/ssl/private/localhost.key -out /etc/ssl/certs/localhost.crt
+
 
 echo "Configurating Wordpress"
 mkdir /var/www/html/wordpress
@@ -23,9 +22,9 @@ chmod -R 777 /var/www/html/wordpress
 chown -R www-data:www-data /var/www/html/wordpress
 echo "\033[1;32mWordpress [OK]\033[0m"
 
+
 echo "Configure autoindex"
 mv /var/www/html/index.nginx-debian.html /temp/index.html
-
 
 
 echo "Configurating Nginx"
@@ -43,6 +42,7 @@ service mysql start
 service php7.3-fpm start
 echo "\033[1;32mServices started\033[0m"
 
+
 echo "Creating Database"
 echo "CREATE DATABASE MyDatabase" | mysql -u root
 echo "GRANT ALL PRIVILEGES ON MyDatabase.* TO 'root'@'localhost';" | mysql -u root
@@ -51,11 +51,6 @@ echo "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user='root';"
 echo "\033[1;32mDatabase Created\033[0m"
 mysql -u root MyDatabase < temp/MyDatabase.sql 
 
-
-#mysql -u root
-#quit
-# mysql wordpress -u root --password= < /tmp/wordpress.sql
-#mysql -u root < /var/www/html/html/phpmyadmin/sql/create_tables.sql
 
 echo "=============Scritp ended=============="
 bash
